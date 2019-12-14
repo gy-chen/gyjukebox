6717dc8f50330ee094c3cc15d4476f21e3e82c99
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-12-14 13:26:10 +0000

    fix skipping track bug

--- gyjukebox/spotify/player.py
+++ gyjukebox/spotify/player.py
@@ -1,8 +1,12 @@
 import queue
 import time
+import logging
+import threading
 import spotify
 from spotify.player import PlayerState
 from spotify.utils import EventEmitter
+
+logger = logging.getLogger(__name__)
 
 
 class NextTrackQueue:
@@ -15,6 +19,7 @@ class NextTrackQueue:
         Args:
             request_track: gyjukebox.spotify.model.RequestTrack
         """
+        logger.info("Add track %s", request_track)
         self._queue.put(request_track)
 
     def next_track(self):
@@ -24,8 +29,10 @@ class NextTrackQueue:
             gyjukebox.spotify.model.RequestTrack
         """
         try:
+            logger.info("Try to next tract")
             return self._queue.get(False)
         except queue.Empty:
+            logger.info("No next track")
             raise _NoNextTrackError()
 
     def size(self):
@@ -46,6 +53,7 @@ class Player(EventEmitter):
         self._queue = queue
         self._playing_track = None
         self._track_start_timestamp = None
+        self._changing_song_lock = threading.Lock()
 
         session.on(spotify.SessionEvent.END_OF_TRACK, self._on_end_of_track)
 
@@ -54,18 +62,31 @@ class Player(EventEmitter):
 
         if player is paused, resume playing, otherwise load and play next track.
         """
+        logger.info("Try to start player")
         if self._session.player.state == PlayerState.PLAYING:
+            logger.info("Player is in PLAYING state, do nothing.")
             return
         if self._session.player.state != PlayerState.PAUSED:
-            try:
-                self._playing_track = self._load_next_track()
-            except _NoNextTrackError:
-                self._track_start_timestamp = None
-                self._playing_track = None
+            if not self._changing_song_lock.acquire(blocking=False):
+                logger.info("cannot acquire changing song lock, do nothing")
                 return
-            self._track_start_timestamp = time.time()
-            self.emit('next_track', self._playing_track, self._track_start_timestamp)
-        self._session.player.play()
+            try:
+                try:
+                    self._playing_track = self._load_next_track()
+                    logger.info("Got next track %s", self._playing_track)
+                except _NoNextTrackError:
+                    self._track_start_timestamp = None
+                    self._playing_track = None
+                    return
+                self._track_start_timestamp = time.time()
+                self.emit(
+                    "next_track", self._playing_track, self._track_start_timestamp
+                )
+                self._session.player.play()
+            finally:
+                self._changing_song_lock.release()
+        else:
+            self._session.player.play()
 
     def _load_next_track(self):
         request_track = self._queue.next_track()
@@ -80,23 +101,33 @@ class Player(EventEmitter):
         return self._track_start_timestamp
 
     def _on_end_of_track(self, _):
-        self.emit('end_of_track', self._playing_track)
-        self._track_start_timestamp = None
-        try:
-            self._playing_track = self._load_next_track()
-        except _NoNextTrackError:
-            self._track_start_timestamp = None
-            self._playing_track = None
+        self.emit("end_of_track", self._playing_track)
+        logger.info("End of track")
+        logger.info("Current player state: %s", self._session.player.state)
+        if not self._changing_song_lock.acquire(blocking=False):
+            logger.info("cannot acquire changing song lock, do nothing")
             return
-        self._track_start_timestamp = time.time()
-        self.emit('next_track', self._playing_track, self._track_start_timestamp)
-        self._session.player.play()
+        try:
+            self._track_start_timestamp = None
+            try:
+                self._playing_track = self._load_next_track()
+                logger.info("Got next track %s", self._playing_track)
+            except _NoNextTrackError:
+                self._track_start_timestamp = None
+                self._playing_track = None
+                return
+            self._track_start_timestamp = time.time()
+            self.emit("next_track", self._playing_track, self._track_start_timestamp)
+            self._session.player.play()
+        finally:
+            self._changing_song_lock.release()
 
     def on_end_of_track(self, callback, *user_args):
-        self.on('end_of_track', callback, *user_args)
+        self.on("end_of_track", callback, *user_args)
 
     def on_next_track(self, callback, *user_args):
-        self.on('next_track', callback, *user_args)
+        self.on("next_track", callback, *user_args)
+
 
 class _NoNextTrackError(Exception):
     pass
