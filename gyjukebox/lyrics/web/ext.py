7b5a9d5df094841ab20bcc146710eed531af1d40
GYCHEN <gy.chen@gms.nutc.edu.tw>
2020-01-09 10:58:27 +0100

    Revert "share searcher"
    This reverts commit d1252be221317e9ccd705ad51c2a8e240a441c90.

--- gyjukebox/lyrics/web/ext.py
+++ gyjukebox/lyrics/web/ext.py
@@ -1,5 +1,5 @@
-import collections
 from flask import current_app
+from flask import _app_ctx_stack
 from gyjukebox.lyrics.nlp.pure.index import Indexer
 from gyjukebox.lyrics.nlp.pure.index import FileIndexDataWriter
 from gyjukebox.lyrics.nlp.pure.index import FileIndexDataReader
@@ -7,9 +7,6 @@ from gyjukebox.lyrics.nlp.pure.index import FileIndexPerDocumentWriter
 from gyjukebox.lyrics.nlp.pure.index import FileIndexPerDocumentReader
 from gyjukebox.lyrics.nlp.pure.docs import LyricsDocs
 from gyjukebox.lyrics.search import PureNlpLyricsSearcher
-
-
-_LyricsSearchExtConfig = collections.namedtuple("_LyricsSearchExtConfig", "searcher")
 
 
 class LyricsSearchExt:
@@ -21,13 +18,6 @@ class LyricsSearchExt:
     def init_app(self, app):
         app.config.setdefault("LYRICS_SEARCH_LYRICS_PATH", None)
         app.config.setdefault("LYRICS_SEARCH_INDEX_PATH", None)
-
-        lyrics_path = app.config["LYRICS_SEARCH_LYRICS_PATH"]
-        index_path = app.config["LYRICS_SEARCH_INDEX_PATH"]
-        searcher = PureNlpLyricsSearcher(lyrics_path, index_path)
-
-        config = _LyricsSearchExtConfig(searcher)
-        app.extensions["lyrics_search_ext"] = config
 
     def index(self):
         lyrics_path = self.app.config["LYRICS_SEARCH_LYRICS_PATH"]
@@ -44,7 +34,15 @@ class LyricsSearchExt:
 
     @property
     def searcher(self):
-        return self.app.extensions["lyrics_search_ext"].searcher
+        ctx = _app_ctx_stack.top
+        if ctx is not None:
+            if not hasattr(ctx, "lyrics_search_ext_searcher"):
+                lyrics_path = self.app.config["LYRICS_SEARCH_LYRICS_PATH"]
+                index_path = self.app.config["LYRICS_SEARCH_INDEX_PATH"]
+                ctx.lyrics_search_ext_searcher = PureNlpLyricsSearcher(
+                    lyrics_path, index_path
+                )
+            return ctx.lyrics_search_ext_searcher
 
     @property
     def app(self):
